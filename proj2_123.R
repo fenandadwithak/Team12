0854d163f36c385be504e45571ad97166de5b19e
nurmawiyaa <nurmawiya@gmail.com>
2025-10-07 10:04:11 +0100

    Update proj2_123.R

--- proj2_123.R
+++ proj2_123.R
@@ -1,11 +1,10 @@
-# 1) Create household assignment
+# 1) Create n people assigned to corresponding household where maximum size is 5
 n <- 1000 #population size
 hmax <- 5 #maximum household size
-#number of households with size uniformly distributed of max = 5
-hh_n <- ceiling(n/((hmax+1)/2))
 #creating n vector of which the number refers to household
-#and the occurence of the number refers to size of corresponding household
-h <- rep(1:hh_n, times = sample(1:hmax, hh_n, replace = TRUE))[1:n]
+#and the occurence frequency refers to size of corresponding household
+prob <- rep (1/hmax, hmax) #probability of discrete uniform for each size
+h <- rep(1:n, times = sample(1:hmax, n, replace = TRUE, prob = prob))[1:n]
 
 # 2) Contact network
 #nc = average number of contacts per person
@@ -29,9 +28,9 @@ get.net <- function (beta, nc=15, h) {
   return(links)
 }
 
-#3 nseir function
-nseir<-function(beta,h,alink,alpha=c(.1,.01,.01),
-                delta=.2,gamma=.4,nc=15,nt=100,pinf=.005){
+# 3) nseir function
+nseir<-function(beta, h, alink, alpha=c(.1,.01,.01),
+                delta=.2, gamma=.4, nc=15, nt=100, pinf=.005){
   n <- length(beta)
   state <- rep("S", n)
   state[sample(1:n, pinf * n)] <- "I"
@@ -71,3 +70,40 @@ nseir<-function(beta,h,alink,alpha=c(.1,.01,.01),
   
   return(data.frame(t = 1:nt, res))
 }
+
+# 4) Function Plot
+plot.seir <- function(res, title = "SEIR Dynamics") {
+  plot(res$t, res$S, type = "l", ylim = c(0, max(res$S)),
+       xlab = "Days", ylab = "Number of individuals",
+       col = "blue", lwd = 2, main = title)
+  lines(res$t, res$E, col = "orange", lwd = 2)
+  lines(res$t, res$I, col = "red", lwd = 2)
+  lines(res$t, res$R, col = "darkgreen", lwd = 2)
+  legend("right", legend = c("S", "E", "I", "R"),
+         col = c("blue", "orange", "red", "darkgreen"), lwd = 2)
+}
+
+# 5) Compare 
+set.seed(123)
+
+beta <- runif(n)
+alink <- get.net(beta, nc = 15, h = h)
+
+# Scenario 1: Full model
+res1 <- nseir(beta, h, alink)
+
+# Scenario 2: Random mixing only
+res2 <- nseir(beta, h, alink, alpha = c(0, 0, 0.04))
+
+# Scenario 3: Constant beta
+res3 <- nseir(rep(mean(beta), n), h, alink)
+
+# Scenario 4: Constant beta + random mixing
+res4 <- nseir(rep(mean(beta), n), h, alink, alpha = c(0, 0, 0.04))
+
+# Plot all scenarios side by side
+par(mfrow = c(2, 2))
+plot.seir(res1, "Full Model")
+plot.seir(res2, "Random Mixing Only")
+plot.seir(res3, "Constant Beta")
+plot.seir(res4, "Constant Beta + Random Mixing")
