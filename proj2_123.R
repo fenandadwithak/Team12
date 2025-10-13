1b22d3369d07274c035c4ce501253a9dbd07e860
nurmawiyaa <nurmawiya@gmail.com>
2025-10-13 17:04:01 +0100

    Update proj2_123.R

--- proj2_123.R
+++ proj2_123.R
@@ -1,70 +1,93 @@
 # 1) Create n people assigned to corresponding household where maximum size is 5
 n <- 1000 #population size
 hmax <- 5 #maximum household size
+
 #creating n vector of which the number refers to household
 #and the occurences refers to size of corresponding household
 set.seed(3)
-h <- rep(1:n, times=sample(1:hmax, n, replace=TRUE))[1:n]
+h <- rep(1:n, times = sample(1:hmax, n, replace=TRUE))[1:n]
 
 # 2) Contact network
-#nc = average number of contacts per person
-#beta = sociability parameter
+#nc: average number of contacts per person
+#beta: sociability parameter, higher means more likely to form links
 get.net <- function (beta, nc=15, h) {
+  #create links (daily contacts) for each person
   n <- length(beta)
-  beta_bar <- mean(beta)
-  links <- vector("list", n)
+  beta_bar <- mean(beta) #mean sociability parameter
+  links <- vector("list", n) #set null vector for links of each person
   
-  for (i in 1:(n-1)) {
-    for (j in (i+1):n) {
-      if (h[i] != h[j]) { # exclude household members
+  for (i in 1:(n-1)) { #loop over person i from 1 to n-1
+    for (j in (i+1):n) { #loop over person j from i+1 to n to avoid duplicates
+      if (h[i] != h[j]) { #only connect if they are not in the same household
+        #prob that i and j have contact (daily link)
         p_link <- nc * beta[i] * beta[j] / (beta_bar^2 * (n - 1))
+        
+        #If daily link of i and j person > random value U(0,1),
+        #then create a link between i and j
         if (runif(1) < p_link) {
-          links[[i]] <- c(links[[i]], j)
-          links[[j]] <- c(links[[j]], i)
+          links[[i]] <- c(links[[i]], j) #Store j into list who connects with i
+          links[[j]] <- c(links[[j]], i) #Store i in j's list of contacts 
         }
       }
     }
   }
-  return(links)
+  return(links) #return the list of daily contacts for each person
 }
 
 # 3) nseir function
-nseir<-function(beta, h, alink, alpha=c(.1,.01,.01),
-                delta=.2, gamma=.4, nc=15, nt=100, pinf=.005){
-  n <- length(beta)
-  state <- rep("S", n)
-  state[sample(1:n, pinf * n)] <- "I"
+nseir <- function(beta, h, alink, alpha=c(.1, .01, .01),
+                  delta=.2, gamma=.4, nc=15, nt=100, pinf=.005){
+  #SEIR stochastic simulation model
+  #beta: sociability parameter of each person
+  #h: household list
+  #alink: list of regular contacs of each person returned by get.net
+  #alpha[1]: daily prob i infecting j if in same household (alpha h)
+  #alpha[2]: daily prob i infecting j if in regular contact (alpha c)
+  #alpha[3]: daily prob i infecting j through random mixing (alpha r)
+  #delta: daily prob I -> R
+  #gamma: daily prob E -> I
+  #nc: average number of daily contacts for each person
+  #nt: number of days to simulate
+  #pinf: proportion of the initial population to randomly start in the I state
   
-  res <- matrix(0, nrow = nt, ncol = 4)
-  colnames(res) <- c("S", "E", "I", "R")
+  n <- length(beta) #population size
+  state <- rep("S", n) #initialize susceptible
+  state[sample(1:n, pinf * n)] <- "I" #randomly choose initial in the I state
   
-  beta_bar <- mean(beta)
+  res <- matrix(0, nrow = nt, ncol = 4) #set up storage for pop in each state
+  colnames(res) <- c("S", "E", "I", "R") #naming the column
   
-  for (t in 1:nt) {
-    newE <- rep(FALSE, n)
-    newI <- (state == "E") & (runif(n) < gamma)
-    newR <- (state == "I") & (runif(n) < delta)
+  beta_bar <- mean(beta) #mean sociability parameter
+  
+  for (t in 1:nt) { #simulate over nt days
+    newE <- rep(FALSE, n) #to record who becomes exposed
+    #using random deviates: runif(n)
+    newI <- (state == "E") & (runif(n) < gamma) #E -> I with prob gamma
+    newR <- (state == "I") & (runif(n) < delta) #I -> R with prob delta
     
+    #Loop over each infectious person to spread infection
     for (i in which(state == "I")) {
-      # household infections
+      # household infections: infect susceptible in same household
       hh_members <- which(h == h[i] & state == "S")
       newE[hh_members]<-newE[hh_members]|(runif(length(hh_members))< alpha[1])
       
-      # network infections
+      # network infections: infect regular contacts from alink
       contacts <- alink[[i]]
       contacts <- contacts[state[contacts] == "S"]
       newE[contacts] <- newE[contacts] | (runif(length(contacts)) < alpha[2])
       
-      # random mixing
+      # random mixing: infect random susceptibles
       targets <- which(state == "S")
       p_random <- alpha[3]*nc*beta[i]*beta[targets]/(beta_bar^2 * (n - 1))
       newE[targets] <- newE[targets] | (runif(length(targets)) < p_random)
     }
     
+    #update states based on new infections
     state[newE & state == "S"] <- "E"
     state[newI] <- "I"
     state[newR] <- "R"
     
+    #record daily counts of each state
     res[t, ] <- table(factor(state, levels = c("S", "E", "I", "R")))
   }
   
@@ -86,8 +109,9 @@ plot.seir <- function(res, title = "SEIR Dynamics") {
 # 5) Compare 
 set.seed(123)
 
-beta <- runif(n)
-alink <- get.net(beta, nc = 15, h = h)
+beta <- runif(n, 0, 1) #setting beta vector ~ U(0, 1)
+alink <- get.net(beta, nc = 15, h = h) #list of regular contacts of each person
+ave_check <- mean(sapply(alink, length)) #checking if mean contacts close to nc
 
 # Scenario 1: Full model
 res1 <- nseir(beta, h, alink)
