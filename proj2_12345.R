56f6b35fbf6b90e5f3840b1cdeca571fa6af265d
fenandadwithak <s2744048@ed.ac.uk>
2025-10-15 00:51:54 +0100

    Update proj2_12345.R

--- proj2_12345.R
+++ proj2_12345.R
@@ -6,11 +6,11 @@
 # Nurmawiya : S2822251
 
 # Aseel : create n people and its household, contact network, cross-check 
-#         the entire code & revise comments
+#         the entire code and revise code & comments
 # Fenanda : create plot function, compare the model, cross-check the entire code
-#           and revise comments
+#           and revise code & comments
 # Nurmawiya : create nseir function, cross-check code, cross-check the entire 
-#             code and revise comments
+#             code and revise code & comments
 
 # SEIR (Susceptible, Exposed, Infections, Recovered) Model Simulation
 # Goals : create a model and how its use to investigate the role of household
@@ -27,32 +27,79 @@
   h <- rep(1:length(sizes), times = sizes)[1:n]
 
 # 2) Contact network
-get.net <- function (beta, nc=15, h) {
-  # Function to create links (daily contacts) for each person
-  # nc: average number of contacts per person
-  # beta: sociability parameter, higher means more likely to form links
-  
-  # output: list of connected networks from each person
-  n <- length(beta)
-  beta_bar <- mean(beta) #mean sociability parameter
-  links <- vector("list", n) #set null vector for links of each person
-  
-  for (i in 1:(n-1)) { #loop over person i from 1 to n-1
-    for (j in (i+1):n) { #loop over person j from i+1 to n to avoid duplicates
-      if (h[i] != h[j]) { #only connect if they are not in the same household
-        #prob that i and j have contact (daily link)
-        p_link <- nc * beta[i] * beta[j] / (beta_bar^2 * (n - 1))
+  get.net <- function (beta, nc=15, h) {
+    # Function to create links (daily contacts) for each person
+    
+    # nc: average number of contacts per person, set to 15
+    # beta: sociability parameter, higher means more likely to form links
+    # OUTPUT : list of connected networks from each person (output.net)
+    
+    n <- length(beta) ## calculate total obs of beta, or equal to total population
+    beta_bar <- mean(beta) ## calculate mean(beta)/"sociability parameter"
+    
+    # Allocate the possible connection formed in each individual
+    
+    output.net <- vector("list", n) ## list to store the result
+    output.net0 <- integer(n) ## vector to store number of contact made per idv
+    
+    # Create list of n elements which in each element has "ub_nc" sub-element
+    # Later, we would to allocate (drop/add sub-element and store possible 
+    # contacts until overall number of contacts achieved (nc=15)
+    for (k in 1:n) output.net[[k]] <- integer(nc) ## set initial value output.net
+    
+    # Loop over each individual
+    for (i in 1:(n-1)) {
+      j <- (i+1):n # candidates contacts were generated
+      flag <- h[i] != h[j]  # "FALSE" if i & j are belong to same household
+      # means that pairing wouldn't be generated
+      j <- j[flag] # filter candidates contacts from different household
+      
+      if (length(j) == 0) next # skip iteration when there are no candidate
+      
+      # Probability of  a link between persons i and j 
+      p_link <- nc * beta[i] * beta[j] / (beta_bar^2 * (n - 1))
+      
+      # Select the candidates contacts based on p-link
+      # linked is a vector containing possible candidate contacts
+      linked <- j[runif(length(j)) < p_link]
+      
+      # If there is chosen candidate(s), store it into output.net and output.net0
+      if (length(linked) > 0) {
+        # Position to fill
+        pos = (output.net0[i]+1):(output.net0[i]+length(linked))
         
-        #If random value U(0,1) < p_link, create a link between i and j
-        if (runif(1) < p_link) {
-          links[[i]] <- c(links[[i]], j) #Store j into list who connects with i
-          links[[j]] <- c(links[[j]], i) #Store i in j's list of contacts 
+        # Assign link[[i]] with number of candidates stored in "linked" at the
+        # defined position
+        output.net[[i]][pos] <- linked
+        
+        # Assign number of chosen candidates at i index in output.net0
+        output.net0[i] <- output.net0[i] + length(linked)
+        
+        # Vice versa, assign "i" to their linked candidates
+        for (j1 in linked) {
+          # Assign i to output.net's of its chosen candidates
+          output.net[[j1]][output.net0[j1]+1] <- i 
+          # Assign the number of candidates (add by 1) to its chosen candidates
+          output.net0[j1] <- output.net0[j1] + 1 
         }
       }
+      ## to the next value of i
     }
-  }
-  return(links) #return the list of daily contacts for each person
-} ## get.net
+    
+    # Drop sub-element/rejected candidates and replace person with no contact 
+    for (k in 1:n) {
+      if (output.net0[k] > 0) { 
+        # if person has "chosen candidates" to link and 
+        # number of chosen candidates < nc, filter "assigned" sub-element 
+        # in "output.net"
+        output.net[[k]] <- output.net[[k]][1:output.net0[k]]
+      } else {
+        # if there is no chosen candidate, replace sub-element into NULL
+        output.net[[k]] <- integer(0)
+      }
+    }
+    return(output.net)
+  } ## get.net
 
 # 3) nseir function
 nseir <- function(beta, h, alink, alpha=c(.1, .01, .01),
