bdd50c29bfffb2acefcee884433f19c51617c97c
nurmawiyaa <nurmawiya@gmail.com>
2025-10-15 01:22:03 +0100

    revised nseir

--- proj2_12345.R
+++ proj2_12345.R
@@ -18,88 +18,88 @@
 
 
 # 1) Create n people assigned to corresponding household where maximum size is 5
-  n <- 1000 #population size
-  hmax <- 5 #maximum household size
-  #creating n vector of which the number refers to household
-  #-and the occurrences refers to size of corresponding household
-  set.seed(3)
-  sizes <- sample(1:hmax, n, replace = TRUE)
-  h <- rep(1:length(sizes), times = sizes)[1:n]
+n <- 1000 #population size
+hmax <- 5 #maximum household size
+#creating n vector of which the number refers to household
+#-and the occurrences refers to size of corresponding household
+set.seed(3)
+sizes <- sample(1:hmax, n, replace = TRUE)
+h <- rep(1:length(sizes), times = sizes)[1:n]
 
 # 2) Contact network
-  get.net <- function (beta, nc=15, h) {
-    # Function to create links (daily contacts) for each person
+get.net <- function (beta, nc=15, h) {
+  # Function to create links (daily contacts) for each person
     
-    # nc: average number of contacts per person, set to 15
-    # beta: sociability parameter, higher means more likely to form links
-    # OUTPUT : list of connected networks from each person (output.net)
+  # nc: average number of contacts per person, set to 15
+  # beta: sociability parameter, higher means more likely to form links
+  # OUTPUT : list of connected networks from each person (output.net)
     
-    n <- length(beta) ## calculate total obs of beta, or equal to total population
-    beta_bar <- mean(beta) ## calculate mean(beta)/"sociability parameter"
+  n <- length(beta) ## calculate total obs of beta, or equal to total population
+  beta_bar <- mean(beta) ## calculate mean(beta)/"sociability parameter"
     
-    # Allocate the possible connection formed in each individual
+  # Allocate the possible connection formed in each individual
     
-    output.net <- vector("list", n) ## list to store the result
-    output.net0 <- integer(n) ## vector to store number of contact made per idv
+  output.net <- vector("list", n) ## list to store the result
+  output.net0 <- integer(n) ## vector to store number of contact made per idv
     
-    # Create list of n elements which in each element has "ub_nc" sub-element
-    # Later, we would to allocate (drop/add sub-element and store possible 
-    # contacts until overall number of contacts achieved (nc=15)
-    for (k in 1:n) output.net[[k]] <- integer(nc) ## set initial value output.net
+  # Create list of n elements which in each element has "ub_nc" sub-element
+  # Later, we would to allocate (drop/add sub-element and store possible 
+  # contacts until overall number of contacts achieved (nc=15)
+  for (k in 1:n) output.net[[k]] <- integer(nc) ## set initial value output.net
     
-    # Loop over each individual
-    for (i in 1:(n-1)) {
-      j <- (i+1):n # candidates contacts were generated
-      flag <- h[i] != h[j]  # "FALSE" if i & j are belong to same household
+  # Loop over each individual
+  for (i in 1:(n-1)) {
+    j <- (i+1):n # candidates contacts were generated
+    flag <- h[i] != h[j]  # "FALSE" if i & j are belong to same household
       # means that pairing wouldn't be generated
-      j <- j[flag] # filter candidates contacts from different household
+    j <- j[flag] # filter candidates contacts from different household
       
-      if (length(j) == 0) next # skip iteration when there are no candidate
+    if (length(j) == 0) next # skip iteration when there are no candidate
       
-      # Probability of  a link between persons i and j 
-      p_link <- nc * beta[i] * beta[j] / (beta_bar^2 * (n - 1))
+    # Probability of  a link between persons i and j 
+    p_link <- nc * beta[i] * beta[j] / (beta_bar^2 * (n - 1))
       
-      # Select the candidates contacts based on p-link
-      # linked is a vector containing possible candidate contacts
-      linked <- j[runif(length(j)) < p_link]
+    # Select the candidates contacts based on p-link
+    # linked is a vector containing possible candidate contacts
+    linked <- j[runif(length(j)) < p_link]
       
-      # If there is chosen candidate(s), store it into output.net and output.net0
-      if (length(linked) > 0) {
-        # Position to fill
-        pos = (output.net0[i]+1):(output.net0[i]+length(linked))
+    # If there is chosen candidate(s), store it into output.net and output.net0
+    if (length(linked) > 0) {
+      # Position to fill
+      pos = (output.net0[i]+1):(output.net0[i]+length(linked))
         
-        # Assign link[[i]] with number of candidates stored in "linked" at the
-        # defined position
-        output.net[[i]][pos] <- linked
+      # Assign link[[i]] with number of candidates stored in "linked" at the
+      # defined position
+      output.net[[i]][pos] <- linked
         
-        # Assign number of chosen candidates at i index in output.net0
-        output.net0[i] <- output.net0[i] + length(linked)
+      # Assign number of chosen candidates at i index in output.net0
+      output.net0[i] <- output.net0[i] + length(linked)
         
-        # Vice versa, assign "i" to their linked candidates
-        for (j1 in linked) {
-          # Assign i to output.net's of its chosen candidates
-          output.net[[j1]][output.net0[j1]+1] <- i 
-          # Assign the number of candidates (add by 1) to its chosen candidates
-          output.net0[j1] <- output.net0[j1] + 1 
-        }
+      # Vice versa, assign "i" to their linked candidates
+      for (j1 in linked) {
+        # Assign i to output.net's of its chosen candidates
+        output.net[[j1]][output.net0[j1]+1] <- i 
+        # Assign the number of candidates (add by 1) to its chosen candidates
+        output.net0[j1] <- output.net0[j1] + 1 
       }
-      ## to the next value of i
     }
+    ## to the next value of i
+  }
     
-    # Drop sub-element/rejected candidates and replace person with no contact 
-    for (k in 1:n) {
-      if (output.net0[k] > 0) { 
-        # if person has "chosen candidates" to link and 
-        # number of chosen candidates < nc, filter "assigned" sub-element 
-        # in "output.net"
-        output.net[[k]] <- output.net[[k]][1:output.net0[k]]
-      } else {
-        # if there is no chosen candidate, replace sub-element into NULL
-        output.net[[k]] <- integer(0)
-      }
+  # Drop sub-element/rejected candidates and replace person with no contact 
+  for (k in 1:n) {
+    if (output.net0[k] > 0) { 
+      # if person has "chosen candidates" to link and 
+      # number of chosen candidates < nc, filter "assigned" sub-element 
+      # in "output.net"
+      output.net[[k]] <- output.net[[k]][1:output.net0[k]]
+    } else {
+      # if there is no chosen candidate, replace sub-element into NULL
+      output.net[[k]] <- integer(0)
     }
-    return(output.net)
-  } ## get.net
+  }
+  return(output.net)
+} ## get.net
 
 # 3) nseir function
 nseir <- function(beta, h, alink, alpha=c(.1, .01, .01),
@@ -126,53 +126,67 @@ nseir <- function(beta, h, alink, alpha=c(.1, .01, .01),
   
   n <- length(beta) #population size
   state <- rep("S", n) #initialize susceptible
-  state[sample(1:n, pinf * n)] <- "I" #randomly choose initial in the I state
+  state[sample(1:n, round(pinf * n))] <- "I" #randomly choose initial in the I
   
-  res <- matrix(0, nrow = nt, ncol = 4) #set up storage for pop in each state
-  colnames(res) <- c("S", "E", "I", "R") #naming the column
+  seir <- matrix(0, nrow = nt, ncol = 4) #set up storage for pop in each state
+  colnames(seir) <- c("S", "E", "I", "R") #naming the column
   
   beta_bar <- mean(beta) #mean sociability parameter
   
-  for (t in 1:nt) { #simulate over nt days
-    newE <- rep(FALSE, n) #to record who becomes exposed
+  for (t in 1:nt) {
+    #simulate over nt days
+    newE <- logical(n) #to record who becomes exposed
     #using random deviates: runif(n)
     newI <- (state == "E") & (runif(n) < gamma) #E -> I with prob gamma
     newR <- (state == "I") & (runif(n) < delta) #I -> R with prob delta
+    inf <- which(state == "I") #locating infectious
     
     #Loop over each infectious person to spread infection
-    for (i in which(state == "I")) {
+    for (i in inf) {
       # household infections: infect susceptible in same household
-      hh_members <- which(h == h[i] & state == "S")
-      #using or logical to accumulate infections over the loop
-      newE[hh_members] <- newE[hh_members]|(runif(length(hh_members))< alpha[1])
+      hh <- which(h == h[i]) #find everyone in the same household
+      sus_hh <- hh[state[hh] == "S"] #keep only those who are susceptible
+      if (length(sus_hh)) { #will skip if there's no susceptible
+        #store newE for sus_hh if random value < alpha[1]
+        newE[sus_hh] <- newE[sus_hh] | (runif(length(sus_hh)) < alpha[1])
+        #using OR logical to accumulate infections, not to reset them
+      }
       
       # network infections: infect regular contacts from alink
-      contacts <- alink[[i]]
-      contacts <- contacts[state[contacts] == "S"]
-      newE[contacts] <- newE[contacts] | (runif(length(contacts)) < alpha[2])
+      if (length(alink[[i]])) { #will skip if there's no regular contacts
+        #store only the member(s) with regular contacts in S
+        sus_net <- alink[[i]][state[alink[[i]]] == "S"]
+        if (length(sus_net)) { #will skip if there's no sus_net
+          #store newE for sus_net if random value < alpha[2]
+          newE[sus_net] <- newE[sus_net] | (runif(length(sus_net)) < alpha[2])
+        }
+      }
       
       # random mixing: infect random susceptible
-      targets <- which(state == "S")
-      #S -> E
-      p_random <- alpha[3]*nc*beta[i]*beta[targets]/(beta_bar^2 * (n - 1))
-      newE[targets] <- newE[targets] | (runif(length(targets)) < p_random)
+      sus <- which(state == "S") #locating susceptible
+      if (length(sus)) { #will skip if there is no sus
+        #probability irrespective of household or regular contacts
+        p_random <- alpha[3]*nc*beta[i]*beta[sus] / (beta_bar^2*(n-1))
+        #store newE for sus if random value < p_random
+        newE[sus] <- newE[sus] | (runif(length(sus)) < p_random)
+      }
     }
     
     #update states based on new infections
-    state[newE & state == "S"] <- "E"
-    state[newI] <- "I"
-    state[newR] <- "R"
+    state[newE & state == "S"] <- "E" #store susceptible who get exposed
+    state[newI] <- "I" #store exposed to infectious if random value < gamma
+    state[newR] <- "R" #store infectious to recovered if random value < delta
     
     #record daily counts of each state
-    res[t, ] <- table(factor(state, levels = c("S", "E", "I", "R")))
+    seir[t, ] <- tabulate(factor(state, levels=c("S", "E", "I", "R")), nbins=4)
   }
   
   #returns a list of elements S, E, I, R
-  return(data.frame(t = 1:nt, res))
+  return(data.frame(t = 1:nt, seir))
 } ## nseir
 
 # 4) Function Plot
-dyn.plot <- function(res, title = "SEIR Dynamics") {
+dyn.plot <- function(seir, title = "SEIR Dynamics") {
   # Function to create dynamics plot of the simulated population in each states
   
   # x-axis : days to simulate (from 1 to 100)
@@ -189,15 +203,15 @@ dyn.plot <- function(res, title = "SEIR Dynamics") {
 
   # Plot the states in S(susceptible)
   # Create the base plot
-  plot(res$t, res$S, type = "p", ##plot day vs number of susceptible 
-       ylim = c(0, max(res$S)), xlim = c(0, max(res$t)), ## set maximum range
+  plot(seir$t, seir$S, type = "p", ##plot day vs number of susceptible 
+       ylim = c(0, max(seir$S)), xlim = c(0, max(seir$t)), ## set maximum range
        xlab = "Days", ylab = "Number of individuals", ##labelling the plot
        col = "blue", pch = 1, main = title) ##color and type of points in plot
   
   # Add points for each states
-  points(res$t, res$E, col = "orange", pch = 1) ##add states 
-  points(res$t, res$I, col = "red", pch = 1)
-  points(res$t, res$R, col = "darkgreen", pch = 1)
+  points(seir$t, seir$E, col = "orange", pch = 1) ##add states 
+  points(seir$t, seir$I, col = "red", pch = 1)
+  points(seir$t, seir$R, col = "darkgreen", pch = 1)
   
   # Add legend
   legend(
@@ -214,29 +228,28 @@ dyn.plot <- function(res, title = "SEIR Dynamics") {
 
 
 # 5) Comparing Model
-  # Setting up beta vector and alink pairs
-  # beta is vector of n values (n = 1000) ~ U(0,1)
-  # alink contains list of network made per each person 
-  set.seed(345) ## to ensure we get same vector values of beta for all model
-  beta <- runif(n); alink <- get.net(beta, nc = 15, h = h)
+# Setting up beta vector and alink pairs
+# beta is vector of n values (n = 1000) ~ U(0,1)
+# alink contains list of network made per each person 
+set.seed(345) ## to ensure we get same vector values of beta for all model
+beta <- runif(n); alink <- get.net(beta, nc = 15, h = h)
 
-  # Scenario 1: Full model with default parameters
-  res1 <- nseir(beta, h, alink)
+# Scenario 1: Full model with default parameters
+seir1 <- nseir(beta, h, alink)
 
-  # Scenario 2: Random mixing - Setting alpha[1] = alpha[2] = 0 and 
-  #             alpha[3] = 0.04
-  res2 <- nseir(beta, h, alink, alpha = c(0, 0, 0.04))
+# Scenario 2: Random mixing - Setting alpha[1] = alpha[2] = 0 and 
+#             alpha[3] = 0.04
+seir2 <- nseir(beta, h, alink, alpha = c(0, 0, 0.04))
 
-  # Scenario 3: Full model with constant beta (beta vector = average beta)
-  res3 <- nseir(rep(mean(beta), n), h, alink)
+# Scenario 3: Full model with constant beta (beta vector = average beta)
+seir3 <- nseir(rep(mean(beta), n), h, alink)
 
-  # Scenario 4: Constant beta + random mixing
-  res4 <- nseir(rep(mean(beta), n), h, alink, alpha = c(0, 0, 0.04))
+# Scenario 4: Constant beta + random mixing
+seir4 <- nseir(rep(mean(beta), n), h, alink, alpha = c(0, 0, 0.04))
 
-  # Plot all scenarios 
-  par(mfrow = c(2, 2), mar = c(4, 4, 3, 1)) 
-  dyn.plot(res1, "Full Model Default Parameter")
-  dyn.plot(res2, "Random Mixing Only")
-  dyn.plot(res3, "Constant Beta")
-  dyn.plot(res4, "Constant Beta and Random Mixing")
-  
+# Plot all scenarios 
+par(mfrow = c(2, 2), mar = c(4, 4, 3, 1)) 
+dyn.plot(seir1, "Full Model Default Parameter")
+dyn.plot(seir2, "Random Mixing Only")
+dyn.plot(seir3, "Constant Beta")
+dyn.plot(seir4, "Constant Beta and Random Mixing")
